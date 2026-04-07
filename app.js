55f4cdcd7b6f1fd667a1e9278dd5f077a069a94f
Cole Nelson <ctnelson2@wisc.edu>
2026-04-07 16:07:14 -0500

    hw9

---
+++ app.js
@@ -0,0 +1,8 @@
+const booksDivNode = document.getElementById("book-list")
+
+// TODO Fetch the data from the API and add a book
+//      component for each book to the book-list! 
+
+function createBookComponent(bookData) {
+    // TODO Create and return a node for the given bookData!
+}
