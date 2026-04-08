37ad78109729d53f53f0b047d92e9d0dcd60e952
Cole Nelson <37457808+ctnelson1997@users.noreply.github.com>
2026-04-08 11:25:58 -0500

    Rename function createBookComponent to appendBookComponent

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const booksDivNode = document.getElementById("book-list")
 // TODO Fetch the data from the API and add a book
 //      component for each book to the book-list! 
 
-function createBookComponent(bookData) {
+function appendBookComponent(bookData) {
     // TODO Create and return a node for the given bookData!
 }
